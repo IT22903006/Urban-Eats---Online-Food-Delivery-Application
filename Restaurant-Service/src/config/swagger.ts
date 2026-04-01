60575cd8923b4d3f00a5d389c0d574d460d61660
Rusiru Kalendra De Silva <IT22903006@my.sliit.lk>
2026-04-01 13:50:31 +0530

    Implementing Swagger

---
+++ Restaurant-Service/src/config/swagger.ts
@@ -0,0 +1,37 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerOptions: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Restaurant Service API",
+      version: "1.0.0",
+      description: "API documentation for the Restaurant microservice",
+    },
+    servers: [
+      {
+        url: "http://localhost:5001",
+        description: "Local server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts", "./src/models/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+export default swaggerSpec;
